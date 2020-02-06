1e539ee4ed84c4185f884ebcd9835e1c837d8ebf
Alicia Maria Zavala Ferreira <alicia@ku.edu>
2020-02-06 13:32:42 -0600

    Fix search method error

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -39,7 +39,7 @@ bool LinkedList<T>::search(T value) const
 	bool isFound = false;
 	for(int i = 1; i < m_size; i++)
 	{
-		if ( temp->getValue() == T)
+		if ( temp->getValue() == value)
 		{
 			isFound = true;
 			break;
